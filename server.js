6b94ce9c733293c95db79d612e0ab466af53652b
Quentin ROUET <quentinrouet@hotmail.fr>
2018-05-16 13:22:12 +0200

    server db

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db             = require('./config/db');
 const port = 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(db.url, (err, db) => {
   if (err) return console.log(err)
   // Make sure you add the database name and not the collection name
   db = database.db("sampledb")

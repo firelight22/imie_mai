c366be8de0cc45a6faaa34ceee5c451a09d19280
firelight22 <quentinrouet@hotmail.fr>
2018-05-17 10:47:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
 
   // Make sure you add the database name and not the collection name
-<<<<<<< HEAD
-  database = database.db("imie_mai");
-=======
-  database = database.db("local");
->>>>>>> bf84da129b654499d9fae3da57859e41d3007bce
+
+  database = database.db("sampledb");
   require('./app/routes')(app, database);
   app.listen(port, () => {
     console.log('We are live on ' + port);

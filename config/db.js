b80574e341937570b7c376553f83289f21c46d41
firelight22 <quentinrouet@hotmail.fr>
2018-05-17 11:19:55 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ module.exports = {
   //url : "mongodb://userCK0:jAQTxN1v2ROSB2q5@mongodb-messenger-imie.193b.starter-ca-central-1.openshiftapps.com/sampledb"
 
   //url : "mongodb://172.30.138.26:27017"
-  url : "mongodb://172.30.138.26:27017"
+  url : "mongodb://127.0.0.1:27017"
 
 
 };
